ccee4c333c313d7345cde23796f0bbe2e6edc899
AhmadBabatunde <105983187+AhmadBabatunde@users.noreply.github.com>
2024-08-06 12:43:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -36,10 +36,10 @@ def main():
         embedding=embeddings, 
         pinecone_api_key=pinecone_api_key
     )
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", convert_system_message_to_human=True, temperature=0.3)
+    #llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", convert_system_message_to_human=True, temperature=0.3)
 
     # Define the LLM
-  ##llm = HuggingFaceEndpoint(repo_id="togethercomputer/RedPajama-INCITE-Chat-3B-v1", huggingfacehub_api_token=huggingfacehub_api_token)
+    llm = HuggingFaceEndpoint(repo_id="togethercomputer/RedPajama-INCITE-Chat-3B-v1", huggingfacehub_api_token=huggingfacehub_api_token)
     
     #llm = genai.GenerativeModel('gemini-1.5-flash')
     #llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
